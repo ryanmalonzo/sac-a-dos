cf8f41aefb417f2232f4178030c57fe7f6b4003b
Ryan Malonzo <ryan@ryanmalonzo.fr>
2021-09-19 16:11:29 +0200

    Implémente le parsing d'objets depuis un fichier texte

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,34 +1,22 @@
 package main;
 
-import objet.Objet;
 import resolution.Methode;
 import sacados.SacADos;
 
-import java.text.DecimalFormat;
-
 public class Main {
     public static void main(String[] args) {
-        Objet[] objets1 = new Objet[4];
-        objets1[0] = new Objet("Boîte 1", 7.0, 13.0);
-        objets1[1] = new Objet("Boîte 2", 4.0, 12.0);
-        objets1[2] = new Objet("Boîte 3", 3.0, 8.0);
-        objets1[3] = new Objet("Boîte 4", 3.0, 10.0);
+        double poidsMax = Double.parseDouble(args[1]);
+        SacADos sac = new SacADos(args[0], poidsMax);
 
-        SacADos sac1 = new SacADos(objets1, 30);
-        sac1.resoudre(Methode.GLOUTONNE);
+        Methode m;
+        switch (args[2]) {
+            case "gloutonne" -> m = Methode.GLOUTONNE;
+            case "dynamique" -> m = Methode.DYNAMIQUE;
+            case "pse" -> m = Methode.PSE;
+            default -> throw new IllegalStateException("Unexpected value: " + args[2]);
+        }
 
-        Objet[] objets2 = new Objet[5];
-        objets2[0] = new Objet("Balle 1", 10.0, 9.0);
-        objets2[1] = new Objet("Balle 2", 7.0, 12.0);
-        objets2[2] = new Objet("Balle 3", 1.0, 2.0);
-        objets2[3] = new Objet("Balle 4", 3.0, 7.0);
-        objets2[4] = new Objet("Balle 5", 2.0, 5.0);
+        sac.resoudre(m);
 
-        SacADos sac2 = new SacADos(objets2, 15);
-        sac2.resoudre(Methode.GLOUTONNE);
-    }
-
-    public static void quicksort(Objet[] t, int first, int last) {
-        resolution.algorithmes.Quicksort.quicksort(t, first, last);
     }
 }
--- src/sacados/SacADos.java
+++ src/sacados/SacADos.java
@@ -3,16 +3,14 @@ package sacados;
 import objet.Objet;
 import resolution.Methode;
 
-public class SacADos {
-    private String chemin;
-    private double poidsMax;
-    private Objet[] objets;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-    // TODO Supprimer (constructeur temporaire)
-    public SacADos(Objet[] objets, double poidsMax) {
-        this.objets = objets;
-        this.poidsMax = poidsMax;
-    }
+public class SacADos {
+    private final String chemin;
+    private final double poidsMax;
+    private Objet[] objets;
 
     public SacADos(String chemin, double poidsMax) {
         this.chemin = chemin;
@@ -20,11 +18,56 @@ public class SacADos {
     }
 
     public void resoudre(Methode methode) {
+        if (objets == null)
+            parse(chemin);
+
         switch (methode) {
             case GLOUTONNE -> Methode.Gloutonne(this);
             case DYNAMIQUE -> Methode.Dynamique(this);
             case PSE -> Methode.PSE(this);
         }
+    }
+
+    private void parse(String chemin) {
+        try {
+            Scanner sc = new Scanner(new File(chemin));
+
+            int nbObjets = nbLignes(chemin);
+
+            // Stocke les objets
+            Objet[] obj = new Objet[nbObjets];
+            int i = 0;
+            while (sc.hasNextLine()) {
+                String[] s = sc.nextLine().split(" ; ");
+                obj[i] = new Objet(s[0],
+                        Double.parseDouble(s[1]), Double.parseDouble(s[2]));
+                ++i;
+            }
+
+            sc.close();
+            objets = obj;
+        }
+        catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private int nbLignes(String chemin) {
+        int nbLignes = 0;
+
+        try {
+            Scanner sc = new Scanner(new File(chemin));
+            while (sc.hasNextLine()) {
+                sc.nextLine();
+                ++nbLignes;
+            }
+            sc.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return nbLignes;
     }
 
     public double getPoidsMax() {
