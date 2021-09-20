a56eae8eb566f71ea377fc07e04287cd5a43f3ed
Ryan Malonzo <ryan@ryanmalonzo.fr>
2021-09-20 22:34:22 +0200

    Corrige les inversions poids/valeur

--- src/objet/Objet.java
+++ src/objet/Objet.java
@@ -5,22 +5,22 @@ public class Objet implements Comparable<Objet> {
     private final double valeur;
     private final double poids;
 
-    public Objet(String nom, double valeur, double poids) {
+    public Objet(String nom, double poids, double valeur) {
         this.nom = nom;
-        this.valeur = valeur;
         this.poids = poids;
+        this.valeur = valeur;
     }
 
     public String getNom() {
         return nom;
     }
 
-    public double getValeur() {
-        return valeur;
-    }
-
     public double getPoids() {
         return poids;
+    }
+
+    public double getValeur() {
+        return valeur;
     }
 
     @Override
--- src/resolution/methodes/Methode.java
+++ src/resolution/methodes/Methode.java
@@ -11,18 +11,24 @@ public abstract class Methode implements IMethode {
         resolution.algorithmes.Quicksort.quicksort(l, 0, l.size() - 1);
     }
 
-    protected static void afficher(List<Objet> contenu, double valeur, double poids) {
+    /**
+     * Affiche le résultat d'une méthode de résolution du sac à dos
+     * @param contenu Les objets composant le sac à dos à l'issue de la méthode
+     * @param valeur La valeur totale du sac à dos
+     * @param poids Le poids total du sac à dos
+     */
+    protected static void afficher(List<Objet> contenu, double poids, double valeur) {
         DecimalFormat df = new DecimalFormat();
         df.setMinimumFractionDigits(2);
         df.setMaximumFractionDigits(2);
 
         System.out.println("Contenu du sac :");
         for (Objet objet : contenu)
-            System.out.println(objet.getNom() + ", valeur " + df.format(objet.getValeur()) + ", poids " +
-                    df.format(objet.getPoids()));
+            System.out.printf("%s, poids %5s, valeur %5s\n", objet.getNom(),
+                    df.format(objet.getPoids()), df.format(objet.getValeur()));
 
         System.out.println();
-        System.out.println("Valeur totale : " + df.format(valeur) + ", poids total : " + df.format(poids));
+        System.out.println("Poids total : " + df.format(poids) + ", valeur totale : " + df.format(valeur));
         System.out.println();
     }
 }
