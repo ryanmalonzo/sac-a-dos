23493b6c656b301ae25c5aa30402880bb4b930b4
Ryan Malonzo <ryan@ryanmalonzo.fr>
2021-10-06 12:15:37 +0200

    Début programmation PSE

--- src/resolution/methodes/PSE.java
+++ src/resolution/methodes/PSE.java
@@ -1,9 +1,18 @@
 package resolution.methodes;
 
+import resolution.utils.ABR;
 import sacados.SacADos;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PSE extends Methode {
     public void resoudre(SacADos sac) {
-
+        int t = sac.getObjets().size();
+        ABR a = new ABR();
+        a.ajouter(new ArrayList<>()); // initialisation vide
+        for (int i = 1; i <= t; ++i) {
+            a.ajouter(new ArrayList<>(List.of(i)));
+        }
     }
 }
